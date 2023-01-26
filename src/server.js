4d5287dd33d5945550a1bcdf3398a428740baac8
poppy1006 <poppyinfo0@gmail.com>
2023-01-27 00:35:43 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const Note = require('./models/Note');
+
+mongoose.connect("mongodb+srv://poppy:poppy@cluster0.egrx7q7.mongodb.net/notesdb") .then(function(){
+    console.log("db connected")
+});
+
+
+// routes
+app.get("/" ,function(req,res){
+    res.send("index");
+});
+
+app.get("/noteslist/" ,async function(req,res){
+    var notes = await Note.find();
+    res.json(notes);
+    // res.send("notes");
+});
+
+app.get("/noteslist/:userid" ,async function(req,res){
+    var notes = await Note.find({userid: req.params.userid});
+    res.json(notes);
+    // res.send("notes");
+});
+
+app.get("/notesadd" ,async function(req,res){
+
+    const newNote = new Note({
+        id: "0007",
+        userid: "poppy2@gmail.com",
+        title: "test note2",
+        content: "asdfghjk wedfghy wertgyh wsedrfgtyhuj swedrfgty edrfgt "
+    });
+    await newNote.save();
+   const response = {message:"New note created!"}
+    res.json(response);
+    // res.send("notes");
+});
+
+app.listen(5000, function(){
+    console.log("🚀 Server started at PORT:5000")
+});
