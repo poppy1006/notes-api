4f910b0798c95daf5673041b04219186687295ce
poppy1006 <poppyinfo0@gmail.com>
2023-01-27 11:32:18 +0530

    feat: :sparkles: route added

--- src/server.js
+++ src/server.js
@@ -2,30 +2,31 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const Note = require('./models/Note');
+mongoose.set('strictQuery', true);
 
 mongoose.connect("mongodb+srv://poppy:poppy@cluster0.egrx7q7.mongodb.net/notesdb") .then(function(){
     console.log("db connected")
+    
 });
-
 
 // routes
 app.get("/" ,function(req,res){
     res.send("index");
 });
 
-app.get("/noteslist/" ,async function(req,res){
+app.get("/notelist" ,async function(req,res){
     var notes = await Note.find();
     res.json(notes);
     // res.send("notes");
 });
 
-app.get("/noteslist/:userid" ,async function(req,res){
+app.get("/notelist/:userid" ,async function(req,res){
     var notes = await Note.find({userid: req.params.userid});
     res.json(notes);
     // res.send("notes");
 });
 
-app.get("/notesadd" ,async function(req,res){
+app.get("/noteadd" ,async function(req,res){
 
     const newNote = new Note({
         id: "0007",
